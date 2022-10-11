31ffb1abf664ef6f1a279ca353478bdf00d26085
UnKnowNModder <85468238+UnKnowNModder@users.noreply.github.com>
2022-10-11 21:07:14 +0530

    Add files via upload

---
+++ download.py
@@ -0,0 +1,16 @@
+# ba_meta require api 7
+import ba, os, _ba
+
+version = 1.0
+
+def install(url: str, destination: str) -> None:
+    import urllib.request
+    data = urllib.request.urlopen(url)
+    print("True")
+    open(destination, 'w').write(str(data.read().decode()))
+    print('True Again')
+
+# ba_meta export plugin
+class Plugin(ba.Plugin):
+    url = 'https://raw.githubusercontent.com/imayushsaini/Bombsquad-Ballistica-Modded-Server/public-server/dist/ba_root/mods/games/running_bombs.py'
+    install(url, _ba.env()["python_directory_user"] + os.sep+'running_bombs.py')
